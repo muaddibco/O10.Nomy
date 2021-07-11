ddf63ea58430d5c21b9380483319eae17bf3a3dd
Kirill Gandyl <muaddibco@gmail.com>
2021-07-11 13:15:31 +0300

    remove https

--- O10.Nomy/Startup.cs
+++ O10.Nomy/Startup.cs
@@ -110,10 +110,10 @@ namespace O10.Nomy
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             if (!env.IsDevelopment())
             {
