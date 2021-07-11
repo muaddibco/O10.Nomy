7eda28ec2ec3e7a9d10113f84524cbc88c646402
Kirill Gandyl <muaddibco@gmail.com>
2021-07-11 17:11:41 +0300

    authorization removed

--- O10.Nomy/ClientApp/src/app/app.module.ts
+++ O10.Nomy/ClientApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -9,15 +9,10 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
-import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
-import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule } from '@angular/material/grid-list'
 import { UserModule } from '../modules/user/user.module';
 import { ExpertsModule } from '../modules/experts/experts.module'
-import { UserEntryComponent } from '../modules/user/user-entry/user-entry.component';
-import { UserRegistrationComponent } from '../modules/user/user-registration/user-registration.component';
 
 @NgModule({
   declarations: [
@@ -32,15 +27,11 @@ import { UserRegistrationComponent } from '../modules/user/user-registration/use
     HttpClientModule,
     FormsModule,
     MatGridListModule,
-    ApiAuthorizationModule,
     UserModule, ExpertsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' }
     ]),
     BrowserAnimationsModule
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
